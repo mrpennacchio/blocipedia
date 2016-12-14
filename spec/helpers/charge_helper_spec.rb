8a7f821125015c1c2e0a293acf483a203deaaf56
Anthony Pennacchio <mrpennacchio@gmail.com>
2016-12-13 22:45:24 -0500

    fixed wiki tests and pundit WikiPolicy

--- spec/helpers/charge_helper_spec.rb
+++ spec/helpers/charge_helper_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
-
-# Specs in this file have access to a helper object that includes
-# the ChargeHelper. For example:
+# require 'rails_helper'
 #
-# describe ChargeHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
+# # Specs in this file have access to a helper object that includes
+# # the ChargeHelper. For example:
+# #
+# # describe ChargeHelper do
+# #   describe "string concat" do
+# #     it "concats two strings with spaces" do
+# #       expect(helper.concat_strings("this","that")).to eq("this that")
+# #     end
+# #   end
+# # end
+# RSpec.describe ChargeHelper, type: :helper do
 # end
-RSpec.describe ChargeHelper, type: :helper do
-end
