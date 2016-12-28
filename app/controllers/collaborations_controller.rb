34776f7429095dd6b0f07126552a4d2ddca65b6c
Anthony Pennacchio <mrpennacchio@gmail.com>
2016-12-28 10:11:55 -0500

    collaborators are added

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -2,23 +2,30 @@ class CollaborationsController < ApplicationController
 
   def new
     @collaborations = Collaboration.new
+    user = User.all
     authorize @collaborations
   end
 
 
   def create
-    authorize @collaborations
+    authorize :collaboration, :create?
 
+    user_email = params[:email]
+    user_id = User.where(email: user_email).pluck(:id)
+    user = User.where(id: user_id)
     wiki = Wiki.find(params[:wiki_id])
-    @collaborations = wiki.collaborators.build(user_id: params[:user_id])
 
-    if @collaborations.save
-      flash[:alert] = "Collaborator Added"
-      redirect_to wiki
+    if @collaborations = wiki.collaborators << user
+      flash[:notice] = "Collaborator Added"
     else
-      flash[:notice] = "Collaborator unable to be added. Try again"
-      render :show
+      flash[:alert] = "Collaborator unable to be added. Try again"
     end
+      redirect_to wiki
+  end
+
+private
+ def collaborator_params
+    params.require(:collaborations).permit(:user_id, :wiki_id)
   end
 
 end
